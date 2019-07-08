949490faf77c4deec16b0f3e6e1a9be74991bcd4
student <sorush.kh68@gmail.com>
2019-07-08 14:43:39 +0100

    All files copied again.

--- code/security.py
+++ code/security.py
@@ -1,13 +1,13 @@
 from werkzeug.security import safe_str_cmp
-from models.user import UserModel
+from user import User
 
 
 def authenticate(username, password):
-    user = UserModel.find_by_username(username)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return UserModel.find_by_id(user_id)
+    return User.find_by_id(user_id)
